b76cd47a67f37dba229ae52c5e197d9638a7b9e2
Daniel Neis Araujo <danielneis@gmail.com>
2018-11-08 08:52:37 -0200

    fix behat tests + missing settings

--- settings.php
+++ settings.php
@@ -27,6 +27,7 @@ defined('MOODLE_INTERNAL') || die;
 require_once($CFG->dirroot . '/' . $CFG->admin . '/tool/certificate/adminlib.php');
 
 if ($hassiteconfig) {
+    // Verify and Manage templates links under Courses.
     $ADMIN->add('courses', new admin_externalpage('tool_certificate/validate',
                 new lang_string('verifycertificate', 'tool_certificate'),
                 new moodle_url('/admin/tool/certificate/verify_certificate.php')));
@@ -35,15 +36,29 @@ if ($hassiteconfig) {
                 new lang_string('managetemplates', 'tool_certificate'),
                 new moodle_url('/admin/tool/certificate/manage_templates.php')));
 
+    // The category for Certificate settings under Tools.
     $ADMIN->add('tools', new admin_category('tool_certificate', get_string('pluginname', 'tool_certificate')));
 
     $settings = new admin_settingpage('toolcertificatemanagetemplates', new lang_string('settings', 'tool_certificate'));
-    $settings->add(new \tool_certificate\admin_setting_link('tool_certificate/uploadimage',
-        get_string('uploadimage', 'tool_certificate'), get_string('uploadimagedesc', 'tool_certificate'),
-        get_string('uploadimage', 'tool_certificate'), new moodle_url('/tool/certificate/upload_image.php'), ''));
 
     $settings->add(new admin_setting_configcheckbox('tool_certificate/verifyallcertificates', get_string('verifyallcertificates',
         'tool_certificate'), '', '0'));
+
+    $settings->add(new admin_setting_configcheckbox('tool_certificate/showposxy', get_string('verifyallcertificates',
+        'tool_certificate'), '', '0'));
+
+    $settings->add(new admin_setting_configcheckbox('tool_certificate/verifyany', get_string('verifyallcertificates',
+        'tool_certificate'), '', '0'));
+
+    $settings->add(new admin_setting_configcheckbox('tool_certificate/protection_modify', get_string('verifyallcertificates',
+        'tool_certificate'), '', '0'));
+
+    $settings->add(new admin_setting_configcheckbox('tool_certificate/protection_copy', get_string('verifyallcertificates',
+        'tool_certificate'), '', '0'));
+
+    $settings->add(new \tool_certificate\admin_setting_link('tool_certificate/uploadimage',
+        get_string('uploadimage', 'tool_certificate'), get_string('uploadimagedesc', 'tool_certificate'),
+        get_string('uploadimage', 'tool_certificate'), new moodle_url('/tool/certificate/upload_image.php'), ''));
 
     $ADMIN->add('tool_certificate', $settings);
 
--- tests/behat/show_position_x_y.feature
+++ tests/behat/show_position_x_y.feature
@@ -2,37 +2,29 @@
 Feature: Being able to set a site setting to determine whether or not to display the position X and Y fields
   In order to ensure the show position X and Y fields setting works as expected
   As an admin
-  I need to ensure teachers can see the position X and Y fields depending on the site setting
+  I need to ensure admins can see the position X and Y fields depending on the site setting
 
   Background:
-    Given the following "users" exist:
-      | username | firstname | lastname | email                |
-      | teacher1 | Teacher   | 1        | teacher1@example.com |
-    And the following "certificate templates" exist:
-      | name |
-      | Test template 1 |
+    Given the following certificate templates exist:
+      | name | numberofpages |
+      | Certificate 1 | 1 |
 
   Scenario: Adding an element with the show position X and Y setting disabled
-    And I log in as "teacher1"
-    And I am on "Course 1" course homepage
-    And I follow "Certificate 1"
-    And I navigate to "Edit certificate" in current page administration
+    When the following config values are set as admin:
+      | showposxy | 0 | tool_certificate |
+    And I log in as "admin"
+    And I navigate to "Courses > Manage certificate templates" in site administration
+    And I click on "Edit" "link"
     And I add the element "Code" to page "1" of the "Certificate 1" certificate template
     And I should not see "Position X"
     And I should not see "Position Y"
 
   Scenario: Adding an element with the show position X and Y setting enabled
+    When the following config values are set as admin:
+      | showposxy | 1 | tool_certificate |
     And I log in as "admin"
-    And I navigate to "Plugins" in site administration
-    And I follow "Manage activities"
-    And I click on "Settings" "link" in the "Custom certificate" "table_row"
-    And I set the field "Show position X and Y" to "1"
-    And I press "Save changes"
-    And I log out
-    And I log in as "teacher1"
-    And I am on "Course 1" course homepage
-    And I follow "Certificate 1"
-    And I navigate to "Edit certificate" in current page administration
+    And I navigate to "Courses > Manage certificate templates" in site administration
+    And I click on "Edit" "link"
     And I add the element "Code" to page "1" of the "Certificate 1" certificate template
     And I should see "Position X"
     And I should see "Position Y"
