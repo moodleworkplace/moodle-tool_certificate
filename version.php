56244a7262a69e0326fc7cccf993adf2bb800969
Daniel Neis Araujo <danielneis@gmail.com>
2018-11-12 15:53:18 -0200

    new capabilities for tenants

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2018051706; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2018051707; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700; // Requires this Moodle version (3.5).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'tool_certificate';
