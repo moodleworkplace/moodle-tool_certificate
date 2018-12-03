88415509157c697a0dc044349d1e0e40fb382c4b
Daniel Neis Araujo <danielneis@gmail.com>
2018-12-03 10:44:37 -0200

    add simple test to code element render_html

---
+++ element/code/tests/element_test.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for code element.
+ *
+ * @package    tool_certificate
+ * @category   test
+ * @copyright  2018 Daniel Neis Araujo <daniel@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Unit tests for code element.
+ *
+ * @package    tool_certificate
+ * @group      tool_certificate
+ * @copyright  2018 Daniel Neis Araujo <daniel@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_certificate_code_element_test_testcase extends advanced_testcase {
+
+    /**
+     * Test set up.
+     */
+    public function setUp() {
+        $this->resetAfterTest();
+    }
+
+    /**
+     * Get tenant generator
+     * @return tool_tenant_generator
+     */
+    protected function get_generator() : tool_certificate_generator {
+        return $this->getDataGenerator()->get_plugin_generator('tool_certificate');
+    }
+
+    public function test_render_html_content() {
+        $certificate1 = $this->get_generator()->create_template((object)['name' => 'Certificate 1']);
+        $pageid = $certificate1->add_page();
+        $element = $certificate1->new_element_for_page_id($pageid, 'code');
+        $e = \tool_certificate\element_factory::get_element_instance($element);
+        $this->assertFalse(empty($e->render_html()));
+    }
+}
