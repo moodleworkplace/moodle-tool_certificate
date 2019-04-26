f583cf24da30c0260947a95eaf4ceba860f0bc3e
Marina Glancy <marina@moodle.com>
2019-04-26 09:42:00 +0200

    SP-532

---
+++ tests/lib_test.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing tests for functions in lib.php
+ *
+ * @package     tool_certificate
+ * @category    test
+ * @copyright   2019 Marina Glancy
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tests for functions in lib.php
+ *
+ * @package    tool_certificate
+ * @copyright  2019 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_certificate_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test for callback 'wp_registration_stats'
+     */
+    public function test_registration_get_site_info() {
+        global $CFG;
+        $this->resetAfterTest(true);
+        $origsiteinfo = $siteinfo = ['moodlerelease' => $CFG->release, 'url' => $CFG->wwwroot];
+        component_callback('tool_wp', 'registration_stats', [&$siteinfo, false]);
+        $appended = array_diff_key($siteinfo, $origsiteinfo);
+
+        $this->assertNotNull($appended['wpcertificates']);
+        $this->assertNotNull($appended['wpcertificatesissues']);
+
+        $siteinfo = $origsiteinfo;
+        component_callback('tool_wp', 'registration_stats', [&$siteinfo, true]);
+    }
+}
