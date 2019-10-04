7fec1db5c402f96c7169c54e95e0499986e2fd35
Paul Holden <paulh@moodle.com>
2019-10-04 13:26:19 +0100

    WP-1126 correct/improve form validation strings.

--- element/digitalsignature/lang/en/certificateelement_digitalsignature.php
+++ element/digitalsignature/lang/en/certificateelement_digitalsignature.php
@@ -23,6 +23,9 @@
  */
 
 $string['digitalsignature'] = 'Digital signature';
+$string['errormultipleimages'] = 'You can only select one image (either uploaded or shared)';
+$string['height'] = 'Height';
+$string['height_help'] = 'Height of the element in mm. The value can be automatically calculated for uploaded/shared images if set to 0, otherwise it must be specified';
 $string['nosignature'] = 'No signature';
 $string['pluginname'] = 'Digital signature';
 $string['privacy:metadata'] = 'The Digital signature plugin does not store any personal data.';
@@ -32,3 +35,5 @@ $string['signaturename'] = 'Signature name';
 $string['signaturepassword'] = 'Signature password';
 $string['signaturereason'] = 'Signature reason';
 $string['uploaddigitalsignature'] = 'Upload digital signature';
+$string['width'] = 'Width';
+$string['width_help'] = 'Width of the element in mm. The value can be automatically calculated for uploaded/shared images if set to 0, otherwise it must be specified';
