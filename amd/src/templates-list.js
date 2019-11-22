9b78f2fd54e63354d60d4deb7c2ad420d36bf5b8
Marina Glancy <marina@moodle.com>
2019-11-22 18:02:58 +0100

    WP-1095 peer review comments addressed

--- amd/src/templates-list.js
+++ amd/src/templates-list.js
@@ -96,11 +96,10 @@ function($, ModalForm, Tabs, Notification, Str, Ajax, WpNotification) {
                     WpNotification.addNotification({message: str, type: 'success'});
                 });
             } else {
-                Str.get_strings([
-                    {key: 'noissueswerecreated', component: 'tool_certificate'}
-                ]).done(function(s) {
-                    WpNotification.addNotification({message: s[0], type: 'warning'});
-                });
+                Str.get_string('noissueswerecreated', 'tool_certificate')
+                    .done(function(s) {
+                        WpNotification.addNotification({message: s, type: 'warning'});
+                    });
             }
         };
     };
