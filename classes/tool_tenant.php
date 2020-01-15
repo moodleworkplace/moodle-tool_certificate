ef8b38dfe2ae483e4ecf84b7ed2df7f658ff7471
Marina Glancy <marina@moodle.com>
2020-01-15 17:26:20 +0100

    WP-1284 list capabilities for admin role

---
+++ classes/tool_tenant.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Implementation of multi-tenancy (used only by plugin tool_tenant and nothing else)
+ *
+ * @package     tool_certificate
+ * @copyright   2020 Moodle Pty Ltd <support@moodle.com>
+ * @author      2020 Marina Glancy
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_certificate;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Implementation of multi-tenancy (used only by plugin tool_tenant and nothing else)
+ *
+ * @package     tool_certificate
+ * @copyright   2020 Moodle Pty Ltd <support@moodle.com>
+ * @author      2020 Marina Glancy
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class tool_tenant {
+
+    /**
+     * Callback used by tool_tenant to see which capabilities from this plugin are allowed for the "Tenant administrator" role
+     *
+     * @return array
+     */
+    public static function get_tenant_admin_capabilities() {
+        return [
+            'tool/certificate:issue' => CAP_ALLOW,
+            'tool/certificate:viewallcertificates' => CAP_ALLOW,
+        ];
+    }
+
+}
