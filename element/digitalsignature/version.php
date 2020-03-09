6f20f1b2feed833c0442a732eec8517dd334341a
Marina Glancy <marina@moodle.com>
2020-03-09 12:53:50 +0100

    Version bump 3.8.2

--- element/digitalsignature/version.php
+++ element/digitalsignature/version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'certificateelement_digitalsignature';
-$plugin->release      = '3.8.1';
-$plugin->version      = 2020012000;
-$plugin->requires     = 2019111801.00;
+$plugin->release      = '3.8.2';
+$plugin->version      = 2020031000;
+$plugin->requires     = 2019111802.00;
 $plugin->maturity     = MATURITY_STABLE;
