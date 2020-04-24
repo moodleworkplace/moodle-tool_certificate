991e3d444add3e453ef550d2ad7f9c18c6f4d322
Mikel Martín <mikel@moodle.com>
2020-04-24 15:56:40 +0200

    WP-1678 Rename the page my_certificates.php to my.php

--- my_certificates.php
+++ my_certificates.php
@@ -25,42 +25,7 @@
 require_once('../../../config.php');
 
 $userid = optional_param('userid', 0, PARAM_INT);
-$download = optional_param('download', null, PARAM_ALPHA);
-$page = optional_param('page', 0, PARAM_INT);
-$perpage = optional_param('perpage', \tool_certificate\certificate::CUSTOMCERT_PER_PAGE, PARAM_INT);
-$pageurl = $url = new moodle_url('/admin/tool/certificate/my_certificates.php', array('userid' => $userid,
-    'page' => $page, 'perpage' => $perpage));
+$pageurl = $url = new moodle_url('/admin/tool/certificate/my.php', ['userid' => $userid]);
 
-// Requires a login.
 require_login();
-
-// Check that we have a valid user.
-$user = \core_user::get_user($userid ?: $USER->id, '*', MUST_EXIST);
-if (!\tool_certificate\permission::can_view_list($user->id)) {
-    throw new \required_capability_exception(context_system::instance(), 'tool/certificate:viewallcertificates',
-        'nopermission', 'error');
-}
-
-$table = new \tool_certificate\my_certificates_table($user->id, $download);
-$table->define_baseurl($pageurl);
-
-if ($table->is_downloading()) {
-    $table->download();
-    exit();
-}
-
-$PAGE->set_url($pageurl);
-$PAGE->set_context(context_user::instance($user->id));
-$PAGE->navigation->extend_for_user($user);
-$PAGE->set_title(get_string('mycertificates', 'tool_certificate'));
-
-$PAGE->set_pagelayout('standard');
-
-$PAGE->navbar->add(get_string('profile'), new moodle_url('/user/profile.php', array('id' => $user->id)));
-$PAGE->navbar->add(get_string('mycertificates', 'tool_certificate'));
-
-echo $OUTPUT->header();
-echo $OUTPUT->heading(get_string('mycertificates', 'tool_certificate'));
-echo html_writer::div(get_string('mycertificatesdescription', 'tool_certificate'));
-$table->out($perpage, false);
-echo $OUTPUT->footer();
+redirect($pageurl);
