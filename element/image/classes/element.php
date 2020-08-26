183a668d3fd67bc19f7e59cbdcc03fa81a8cdca3
Paul Holden <paulh@moodle.com>
2020-08-26 18:13:11 +0100

    WP-2099 handle missing shared images.
    The return typehint for the method is nullable, so ensure a file
    instance is returned otherwise we return boolean false.

--- element/image/classes/element.php
+++ element/image/classes/element.php
@@ -266,9 +266,11 @@ class element extends \tool_certificate\element {
      */
     public function get_shared_file() : ?\stored_file {
         $imageinfo = json_decode($this->get_data());
-        if (!empty($imageinfo->filename)) {
-            return get_file_storage()->get_file($imageinfo->contextid, 'tool_certificate', $imageinfo->filearea, $imageinfo->itemid,
-                $imageinfo->filepath, $imageinfo->filename);
+        if (!empty($imageinfo->filename) &&
+                $file = get_file_storage()->get_file($imageinfo->contextid, 'tool_certificate', $imageinfo->filearea,
+                    $imageinfo->itemid, $imageinfo->filepath, $imageinfo->filename)) {
+
+            return $file;
         }
         return null;
     }
