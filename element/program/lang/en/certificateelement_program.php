7a61e1df5d2f7c75b215ae61c5c485ce2a6b11df
Mikel Martín <mikel@moodle.com>
2020-09-29 09:38:17 +0200

    WP-2039 Rename 'Dynamic rule data' element

--- element/program/lang/en/certificateelement_program.php
+++ element/program/lang/en/certificateelement_program.php
@@ -24,5 +24,5 @@
 
 $string['fieldoptions'] = 'Field';
 $string['fieldoptions_help'] = 'The field to be displayed. If you need to display more than one field, add more instances of this element and configure accordingly.';
-$string['pluginname'] = 'Dynamic rule data';
-$string['privacy:metadata'] = 'The program plugin does not store any personal data.';
+$string['pluginname'] = 'Dynamic fields';
+$string['privacy:metadata'] = 'The dynamic fields plugin does not store any personal data.';
