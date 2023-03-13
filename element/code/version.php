7101a14f32bc3435139767ffbb5c2836764c6351
David Matamoros <davidmc@moodle.com>
2023-03-13 15:17:35 +0100

    Version bump 3.11.13

--- element/code/version.php
+++ element/code/version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'certificateelement_code';
-$plugin->release      = '3.11.12';
-$plugin->version      = 2022031660;
+$plugin->release      = '3.11.13';
+$plugin->version      = 2022031670;
 $plugin->requires     = 2020061502.00;
 $plugin->maturity     = MATURITY_STABLE;
