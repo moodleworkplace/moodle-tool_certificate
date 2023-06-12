95c07b18abb817365391973c4879fef280adc17e
Marina Glancy <marina@moodle.com>
2023-06-12 13:14:32 +0100

    Version bump 3.11.15

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tool_certificate';
-$plugin->release      = '3.11.14';
-$plugin->version      = 2022031680;
+$plugin->release      = '3.11.15';
+$plugin->version      = 2022031690;
 $plugin->requires     = 2020061502.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->supported    = [39, 311];
