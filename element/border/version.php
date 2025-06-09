e1f7278107b7d9012f2233bca38fe7381f1d9e07
Moodle Workplace <workplace@moodle.com>
2025-06-09 12:45:38 +0000

    Version bump 4.5.5

--- element/border/version.php
+++ element/border/version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'certificateelement_border';
-$plugin->release      = '4.5.3';
-$plugin->version      = 2025031800;
+$plugin->release      = '4.5.5';
+$plugin->version      = 2025031802;
 $plugin->requires     = 2022041900.00;
 $plugin->maturity     = MATURITY_STABLE;
