0574a9dcccf4fddbdffe7c28cea6b55246034c54
Moodle Workplace <workplace@moodle.com>
2026-06-08 11:13:00 +0000

    Version bump 5.0.8

--- element/code/version.php
+++ element/code/version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'certificateelement_code';
-$plugin->release      = '5.0.7';
-$plugin->version      = 2026042100;
+$plugin->release      = '5.0.8';
+$plugin->version      = 2026060900;
 $plugin->requires     = 2025041400.00;
 $plugin->maturity     = MATURITY_STABLE;
