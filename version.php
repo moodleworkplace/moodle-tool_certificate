cc670f88b207d17c8f946e962d6dd46d1a0ca58f
Moodle Workplace <workplace@moodle.com>
2026-09-14 10:36:04 +0000

    Version bump 5.0.10

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tool_certificate';
-$plugin->release      = '5.0.9';
-$plugin->version      = 2026081100;
+$plugin->release      = '5.0.10';
+$plugin->version      = 2026091500;
 $plugin->requires     = 2025041400.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->supported    = [500, 502];
